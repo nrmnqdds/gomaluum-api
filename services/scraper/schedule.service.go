5c5fe05ef149fb18873324c89d43a09c7c5407a8
nrmnqdds <quddusnuriman@gmail.com>
2024-09-10 00:10:04 +0800

    Revert ":art: use mutex to prevent race condition"
    This reverts commit 169ab7f367a5d65dc077608fec1430c4ee05accf.

--- services/scraper/schedule.service.go
+++ services/scraper/schedule.service.go
@@ -29,14 +29,9 @@ func ScheduleScraper(e echo.Context) ([]dtos.ScheduleResponse, *dtos.CustomError
 		r.Headers.Set("User-Agent", internal.RandomString())
 	})
 
-	// Target the session dropdown
 	c.OnHTML(".box.box-primary .box-header.with-border .dropdown ul.dropdown-menu li[style*='font-size:16px']", func(e *colly.HTMLElement) {
-		// Get the schedule from the dropdown
 		e.ForEach("a", func(i int, element *colly.HTMLElement) {
 			wg.Add(1)
-
-			// Spawn a goroutine to get the schedule from the session
-			// go getScheduleFromSession(c, element.Attr("href"), element.Text, cookie.Value, &wg, scheduleChan)
 			go getScheduleFromSession(c, element.Attr("href"), element.Text, cookie.Value, &wg)
 		})
 	})
@@ -45,9 +40,7 @@ func ScheduleScraper(e echo.Context) ([]dtos.ScheduleResponse, *dtos.CustomError
 		return nil, dtos.ErrFailedToGoToURL
 	}
 
-	// Wait for all waitgroup to finish
 	wg.Wait()
-
 	return schedule, nil
 }
 
@@ -58,42 +51,36 @@ func getScheduleFromSession(c *colly.Collector, sessionQuery string, sessionName
 
 	subjects := []dtos.Subject{}
 
-	// Target the schedule table
 	c.OnHTML(".box-body table.table.table-hover", func(e *colly.HTMLElement) {
 		e.ForEach("tr", func(i int, element *colly.HTMLElement) {
 			tds := element.ChildTexts("td")
 
 			weekTime := []dtos.WeekTime{}
 
-			// Perfect row
+			if len(tds) == 0 {
+				// Skip the first row
+				return
+			}
+
 			if len(tds) == 9 {
+
 				courseCode := strings.TrimSpace(tds[0])
 				courseName := strings.TrimSpace(tds[1])
-
-				// Convert section string to int
 				section, err := strconv.Atoi(strings.TrimSpace(tds[2]))
 				if err != nil {
 					return
 				}
 
-				// Convert credit hours string to int
 				chr, err := strconv.Atoi(strings.TrimSpace(tds[3]))
 				if err != nil {
 					return
 				}
 
-				// Split the days e.g. "Mon-Fri" -> ["Mon", "Fri"]
 				_days := strings.Split(strings.Replace(strings.TrimSpace(tds[5]), " ", "", -1), "-")
 
 				for _, day := range _days {
-
-					// Get the day number from the day string
-					// e.g. "Mon" -> 1
 					dayNum := internal.GetScheduleDays(day)
-
 					timeTemp := tds[6]
-
-					// Split the time e.g. "0800 - 1000" -> ["0800", "1000"]
 					time := strings.Split(strings.Replace(strings.TrimSpace(timeTemp), " ", "", -1), "-")
 
 					if len(time) != 2 {
@@ -127,27 +114,17 @@ func getScheduleFromSession(c *colly.Collector, sessionQuery string, sessionName
 
 			}
 
-			// Row with merged cells
 			if len(tds) == 4 {
-
-				// Use the last subject to get the course code, course name, section, and credit hours
 				courseCode := subjects[len(subjects)-1].CourseCode
 				courseName := subjects[len(subjects)-1].CourseName
 				section := subjects[len(subjects)-1].Section
 				chr := subjects[len(subjects)-1].Chr
 
-				// Split the days e.g. "Mon-Fri" -> ["Mon", "Fri"]
 				_days := strings.Split(strings.Replace(strings.TrimSpace(tds[0]), " ", "", -1), "-")
 
 				for _, day := range _days {
-
-					// Get the day number from the day string
-					// e.g. "Mon" -> 1
 					dayNum := internal.GetScheduleDays(day)
-
 					timeTemp := tds[1]
-
-					// Split the time e.g. "0800 - 1000" -> ["0800", "1000"]
 					time := strings.Split(strings.Replace(strings.TrimSpace(timeTemp), " ", "", -1), "-")
 
 					if len(time) != 2 {
