eefd5795f53bd01166bebce14ed18bb6088ef72d
nrmnqdds <quddusnuriman@gmail.com>
2024-10-09 14:01:13 +0800

    :bug:

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -68,7 +68,6 @@ func LoginHandler(c echo.Context) error {
 	c.SetCookie(&http.Cookie{
 		Name:  "MOD_AUTH_CAS",
 		Value: data.Token,
-		Path:  "/",
 		// Set the cookie to expire in 1 hour
 		MaxAge: 3600,
 	})
