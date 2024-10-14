0268f1bc7c99f9af4747a736df1f1bb20f721bde
nrmnqdds <quddusnuriman@gmail.com>
2024-10-15 01:12:20 +0800

    :see_no_evil:

--- services/auth/auth.service.go
+++ services/auth/auth.service.go
@@ -17,7 +17,7 @@ var ctx = context.Background()
 func LoginUser(user *dtos.LoginDTO) (*dtos.LoginResponseDTO, *dtos.CustomError) {
 	jar, _ := cookiejar.New(nil)
 
-	opt, _ := redis.ParseURL("redis://default:35a8f8f1b9714c6893b809c8a2ea31e2@allowing-hornet-32763.upstash.io:32763")
+	opt, _ := redis.ParseURL(internal.GetEnv("REDIS_URL"))
 	redisClient := redis.NewClient(opt)
 
 	logger := internal.NewLogger()
