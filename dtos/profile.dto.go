c59cffa9658c73540b654fdeaf7c3fd1fc9f415c
nrmnqdds <quddusnuriman@gmail.com>
2024-11-11 16:00:58 +0800

    :sparkles: add more profile items

--- dtos/profile.dto.go
+++ dtos/profile.dto.go
@@ -1,9 +1,15 @@
 package dtos
 
 type Profile struct {
-	ImageURL string `json:"image_url"`
-	Name     string `json:"name"`
-	MatricNo string `json:"matric_no"`
-	Level    string `json:"level"`
-	Kuliyyah string `json:"kuliyyah"`
+	ImageURL      string `json:"image_url"`
+	Name          string `json:"name"`
+	MatricNo      string `json:"matric_no"`
+	Level         string `json:"level"`
+	Kuliyyah      string `json:"kuliyyah"`
+	IC            string `json:"ic"`
+	Gender        string `json:"gender"`
+	Birthday      string `json:"birthday"`
+	Religion      string `json:"religion"`
+	MaritalStatus string `json:"marital_status"`
+	Address       string `json:"address"`
 }
--- services/scraper/profile.service.go
+++ services/scraper/profile.service.go
@@ -32,12 +32,36 @@ func ProfileScraper(e echo.Context) (*dtos.Profile, *dtos.CustomError) {
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		profile.Name = strings.TrimSpace(e.ChildText(".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='text-align:center; padding:10px; floaf:left;'] h4[style='margin-top:1%;']"))
 
-		profile.Level = strings.TrimSpace(e.ChildText(".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='text-align:center; padding:10px; floaf:left;'] h4:nth-of-type(2)"))
-
 		_matricNo := strings.TrimSpace(e.ChildText(".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='margin-top:3%;'] h4"))
 		profile.MatricNo = strings.TrimSpace(strings.Split(_matricNo, "|")[0])
 
+		profile.Level = strings.TrimSpace(e.ChildText(".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='text-align:center; padding:10px; floaf:left;'] h4:nth-of-type(2)"))
+
 		profile.Kuliyyah = strings.TrimSpace(e.ChildText(".row .col-md-12 .box.box-default .panel-body.row .col-md-4[style='margin-top:3%;'] p"))
+
+		_ic := strings.TrimSpace(e.ChildText(".row:nth-of-type(2) .col-md-12 .nav-tabs-custom .tab-content .tab-pane.active .row .col-md-3 p:nth-of-type(2)"))
+		profile.IC = strings.TrimSpace(strings.Split(_ic, ":")[1])
+
+		_gender := strings.TrimSpace(e.ChildText(".row:nth-of-type(2) .col-md-12 .nav-tabs-custom .tab-content .tab-pane.active .row .col-md-3 p:nth-of-type(3)"))
+		profile.Gender = strings.TrimSpace(strings.Split(_gender, ":")[1])
+
+		_birthday := strings.TrimSpace(e.ChildText(".row:nth-of-type(2) .col-md-12 .nav-tabs-custom .tab-content .tab-pane.active .row .col-md-3 p:nth-of-type(4)"))
+		profile.Birthday = strings.TrimSpace(strings.Split(_birthday, ":")[1])
+
+		_religion := strings.TrimSpace(e.ChildText(".row:nth-of-type(2) .col-md-12 .nav-tabs-custom .tab-content .tab-pane.active .row .col-md-3 p:nth-of-type(5)"))
+		profile.Religion = strings.TrimSpace(strings.Split(_religion, ":")[1])
+
+		_maritalStatus := strings.TrimSpace(e.ChildText(".row:nth-of-type(2) .col-md-12 .nav-tabs-custom .tab-content .tab-pane.active .row .col-md-9 p:nth-of-type(2)"))
+		profile.MaritalStatus = strings.TrimSpace(strings.Split(_maritalStatus, ":")[1])
+
+		_address := strings.TrimSpace(e.ChildText(".row:nth-of-type(2) .col-md-12 .nav-tabs-custom .tab-content .tab-pane.active .row .col-md-9 p:nth-of-type(3)"))
+		// profile.Address = strings.TrimSpace(strings.Split(_address, ":")[1])
+		addressParts := strings.Split(_address, ":")
+		if len(addressParts) > 1 {
+			profile.Address = formatAddress(addressParts[1])
+		} else {
+			profile.Address = addressParts[0]
+		}
 	})
 
 	if err := c.Visit(helpers.ImaluumProfilePage); err != nil {
@@ -51,4 +75,22 @@ func ProfileScraper(e echo.Context) (*dtos.Profile, *dtos.CustomError) {
 	}
 
 	return &profile, nil
+}
+
+func formatAddress(address string) string {
+	// Split the address into lines
+	lines := strings.Split(address, "\n")
+
+	// Clean each line
+	var formattedLines []string
+	for _, line := range lines {
+		// Remove tabs, extra spaces, and trim whitespace
+		cleaned := strings.TrimSpace(strings.ReplaceAll(line, "\t", ""))
+		if cleaned != "" {
+			formattedLines = append(formattedLines, cleaned)
+		}
+	}
+
+	// Join the lines with commas
+	return strings.Join(formattedLines, ", ")
 }
